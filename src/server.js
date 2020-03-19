a84d948171a6c2dbd3b7c8d792c75849dfaf6afa
Bernardo Chagas <bernardochagas@usp.br>
2020-03-19 02:00:18 -0300

    Faz setup inicial do server e do body parser

---
+++ src/server.js
@@ -0,0 +1,12 @@
+import { handleRequest } from './router';
+import http from 'http';
+
+const hostname = process.env.HOST_NAME || '0.0.0.0';
+const port = process.env.API_PORT || 9001;
+
+http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'application/json');
+  handleRequest(req, res);
+}).listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
