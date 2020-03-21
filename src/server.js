75a070cd68241a565e31e58d773efafefb9a7ce9
Bernardo Chagas <bernardochagas@usp.br>
2020-03-20 22:45:04 -0300

    Configura conexão com MongoDB

--- src/server.js
+++ src/server.js
@@ -1,12 +1,28 @@
-import { handleRequest } from './router';
 import http from 'http';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+import { handleRequest } from './router';
 
-const hostname = process.env.HOST_NAME || '0.0.0.0';
-const port = process.env.API_PORT || 9001;
+dotenv.config();
 
-http.createServer((req, res) => {
-  res.setHeader('Content-Type', 'application/json');
-  handleRequest(req, res);
-}).listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+async function startServer() {
+  const { default: db } = await import('./config/db');
+  const { default: apiConfig } = await import('./config/api');
+  const { ENV: env } = process.env;
+
+  try {
+    await mongoose.connect(
+      `mongodb://${db.user}:${db.password}@${db.host}:${db.port}/${db.name}-${env}?authSource=${db.authSource}`,
+      { useNewUrlParser: true, useUnifiedTopology: true });
+  } catch (err) {
+    console.log(err.message);
+  }
+
+  http.createServer(handleRequest)
+  .listen(apiConfig.port, apiConfig.host, () => {
+    const { port, host } = apiConfig;
+    console.log(`Server running at http://${host}:${port}/`);
+  });
+}
+
+startServer();
